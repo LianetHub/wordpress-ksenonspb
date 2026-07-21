745d99009debbf89c6b2ca4f53ce2c7a2e366015
lianet <jack.tolmak@yandex.ru>
2026-07-21 22:46:37 +0300

    feat: add about page

--- template-parts/pages/about.php
+++ template-parts/pages/about.php
@@ -67,38 +67,4 @@ if ($advantages) {
 	);
 }
 
-$video_url = function_exists('get_field') ? (string) get_field('video_url') : '';
-$poster    = function_exists('get_field') ? get_field('video_poster') : null;
-if ($video_url) :
-?>
-	<section class="about-video">
-		<div class="about-video__container container">
-			<h2 class="about-video__title title-md"><?php esc_html_e('Видео о студии', 'ksenonspb'); ?></h2>
-			<div class="about-video__player">
-				<?php if ($poster) : ?>
-					<?php echo ksenon_acf_image($poster, 'large', array('class' => 'about-video__poster')); ?>
-				<?php endif; ?>
-				<a class="about-video__link" href="<?php echo esc_url($video_url); ?>" target="_blank" rel="noopener noreferrer"><?php esc_html_e('Смотреть видео', 'ksenonspb'); ?></a>
-			</div>
-		</div>
-	</section>
-<?php
-endif;
-
-$certificates = function_exists('get_field') ? (array) get_field('certificates') : array();
-if ($certificates) :
-?>
-	<section class="about-certificates">
-		<div class="about-certificates__container container">
-			<h2 class="about-certificates__title title-md"><?php esc_html_e('Сертификаты', 'ksenonspb'); ?></h2>
-			<div class="about-certificates__grid">
-				<?php foreach ($certificates as $image) : ?>
-					<?php echo ksenon_acf_image($image, 'medium', array('class' => 'about-certificates__img')); ?>
-				<?php endforeach; ?>
-			</div>
-		</div>
-	</section>
-<?php
-endif;
-
 get_template_part('template-parts/blocks/cta-form', null, array('variant' => 'free_inspection'));
