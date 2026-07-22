3d6d2fdd00cf29f175a8ca80bb777c1510804638
lianet <jack.tolmak@yandex.ru>
2026-07-22 22:23:56 +0300

    chore: remove old function

--- functions.php
+++ functions.php
@@ -27,4 +27,3 @@ require_once $ksenon_inc . 'enqueue.php';
 require_once $ksenon_inc . 'acf.php';
 require_once $ksenon_inc . 'cpt.php';
 require_once $ksenon_inc . 'cf7.php';
-require_once $ksenon_inc . 'wp-all-import.php';
